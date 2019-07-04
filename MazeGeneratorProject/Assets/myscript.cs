a7baba66fd6584721ea8b89a5df64fbd022166dd
boobypenguin <boobypenguin99@gmail.com>
2019-07-04 15:24:49 +0900

    commit

---
+++ MazeGeneratorProject/Assets/myscript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class myscript : MonoBehaviour
+{
+    Vector3 stdSize;
+    Vector3 smlSize;
+    RaycastHit hit;
+    int counter = 0;
+    bool flg = false;
+
+    void Start()
+    {
+        stdSize = new Vector3(1f, 1f, 1f);
+        smlSize = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+
+    void Update()
+    {
+        if (flg)
+        {
+            if (counter <= 0)
+            {
+                hit.transform.localScale = stdSize;
+                flg = false;
+            }
+            else
+            {
+                counter--;
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = 3.0f;
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            if (!flg)
+            {
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    hit.transform.localScale = smlSize;
+                    counter = 100;
+                    flg = true;
+                }
+                else
+                {
+                    Vector3 newpos = Camera.main.ScreenToWorldPoint(pos);
+                    transform.position = newpos;
+                }
+            }
+        }
+    }
+}
