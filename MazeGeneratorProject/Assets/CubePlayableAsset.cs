b897ce0df568130d7ace0a92bb82debff2715856
boobypenguin <boobypenguin99@gmail.com>
2019-07-05 07:23:44 +0900

    commit

---
+++ MazeGeneratorProject/Assets/CubePlayableAsset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class CubePlayableAsset : PlayableAsset
+{
+
+    public override Playable CreatePlayable(PlayableGraph graph,
+            GameObject go)
+    {
+        CubePlayableBehaviour behaviour = new CubePlayableBehaviour();
+        behaviour.cubeObject = go;
+        ScriptPlayable<CubePlayableBehaviour> playable =
+                ScriptPlayable<CubePlayableBehaviour>.Create(graph,
+                behaviour);
+        return playable;
+    }
+}
---
+++ MazeGeneratorProject/Assets/CubePlayableBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CubePlayableBehaviour : PlayableBehaviour
+{
+    public GameObject cubeObject;
+
+    public override void OnGraphStart(Playable playable) { }
+
+    public override void OnGraphStop(Playable playable) { }
+
+    public override void OnBehaviourPlay(Playable playable,
+            FrameData info)
+    {
+        if (cubeObject == null) { return; }
+        Renderer R = cubeObject.GetComponent<Renderer>();
+        Color c = R.material.color;
+        c.r = 0f;
+        c.g = 1f;
+        R.material.color = c;
+    }
+
+    public override void OnBehaviourPause(Playable playable,
+            FrameData info)
+    {
+        if (cubeObject == null) { return; }
+        Renderer r = cubeObject.GetComponent<Renderer>();
+        Color c = r.material.color;
+        c.r = 1f;
+        c.g = 0f;
+        r.material.color = c;
+    }
+
+    public override void PrepareFrame(Playable playable,
+            FrameData info)
+    { }
+}
